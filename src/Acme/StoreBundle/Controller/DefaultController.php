c73aa54d639669781c882060060b51fe3a425318
robson <robs.cruz@gmail.com>
2015-01-21 18:56:39 -0200

    Teste com client

--- src/Acme/StoreBundle/Controller/DefaultController.php
+++ src/Acme/StoreBundle/Controller/DefaultController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Acme\StoreBundle\Document\Client;
+use Symfony\Component\HttpFoundation\Response;
+
 class DefaultController extends Controller
 {
     /**
@@ -15,5 +18,22 @@ class DefaultController extends Controller
     public function indexAction($name)
     {
         return array('name' => $name);
+    }
+    
+    /**
+     * @Route("/client/{name}")
+     * @Template()
+     */
+    public function createAction()
+    {
+    	$client = new Client();
+    	$client->setName( 'A Foo Bar' );
+    	$client->setAddress( "Rua X, 555 - São Paulo" ) ;
+    
+    	$dm = $this->get('doctrine_mongodb')->getManager();
+    	$dm->persist( $client );
+    	$dm->flush();
+    
+    	return new Response('Created client id '.$client->getId());
     }
 }
--- src/Acme/StoreBundle/Document/Client.php
+++ src/Acme/StoreBundle/Document/Client.php
@@ -23,4 +23,58 @@ class Client
 	 * @MongoDB\String
 	 */
 	protected $address;
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return self
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string $address
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
 }
